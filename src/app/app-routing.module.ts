9af726f086aa153010107f0b68df9264184d944c
VladanStar <vladancupric@gmail.com>
2023-03-24 13:54:06 +0100

    Add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { CarComponent } from './components/car/car.component';
 import { CarDetailsComponent } from './components/car-details/car-details.component';
 import { EditCarComponent } from './components/edit-car/edit-car.component';
+import { AddCarComponent } from './components/add-car/add-car.component';
 
 const routes: Routes = [
-  {path:"", component:CarComponent},
+  { path: '', component: CarComponent },
   { path: 'car/:id', component: CarDetailsComponent },
   { path: 'car/edit/:id', component: EditCarComponent },
+  { path: 'car/add', component: AddCarComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/components/add-car/add-car.component.ts
+++ src/app/components/add-car/add-car.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CarService } from 'src/app/services/car.service';
 
 @Component({
   selector: 'app-add-car',
   templateUrl: './add-car.component.html',
   styleUrls: ['./add-car.component.css']
 })
-export class AddCarComponent {
+export class AddCarComponent implements OnInit{
+
+  constructor(private carService: CarService, private router: Router) {}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  addCar(f: NgForm) {
+    // console.log(f.value)
+
+    this.carService.add(f.value);
+
+    window.alert('Automobil je dodat');
+    this.router.navigate(['/']);
+  }
 
 }
