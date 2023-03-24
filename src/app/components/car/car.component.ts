f39ca125976f127c73accb94490aeb66909d0519
VladanStar <vladancupric@gmail.com>
2023-03-24 09:17:44 +0100

    Add

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CarService } from 'src/app/services/car.service';
 
 @Component({
   selector: 'app-car',
   templateUrl: './car.component.html',
   styleUrls: ['./car.component.css']
 })
-export class CarComponent {
+export class CarComponent implements OnInit {
+  constructor(private carService:CarService){}
+  ngOnInit(): void {
+    this.carService.getAll().subscribe(p=>console.log(p))
+    
+  }
 
 }
