21d03cd317ceaf14b9a321ff6abc78130d3eb4e3
VladanStar <vladancupric@gmail.com>
2023-03-24 13:37:08 +0100

    Add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { CarComponent } from './components/car/car.component';
 import { CarDetailsComponent } from './components/car-details/car-details.component';
 import { EditCarComponent } from './components/edit-car/edit-car.component';
+import { AddCarComponent } from './components/add-car/add-car.component';
 
 const routes: Routes = [
   {path:"", component:CarComponent},
   { path: 'car/:id', component: CarDetailsComponent },
   { path: 'car/edit/:id', component: EditCarComponent },
+  
+  { path: 'car/add', component: AddCarComponent},
 ];
 
 @NgModule({
